7c0989a91294f74ea0e8bff74b5ab85485db946e
Zakariya Hussian <zakariyahussain1989@gmail.com>
2025-09-27 23:23:34 +0600

    Title change

--- script/card6.js
+++ script/card6.js
@@ -20,4 +20,5 @@ document.getElementById("job-btn").addEventListener("click", function(event){
     document.getElementById("activity-history").style.display = "block";
     alert("Bord updated succesfully");
     alert("All Bords updated succesfully");
+    
 })
